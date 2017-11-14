3590e46aade297d1e5558fc8a961122e8708862c
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-14 12:44:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
 
-  const newObj = object.assign ({}, obj);
+  const newObj = Object.assign ({}, obj);
 
   delete newObj.key;
   return newObj;
